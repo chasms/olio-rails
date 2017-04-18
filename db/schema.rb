da323ad581a885ba47e5aac9be35728295e7c0e4
Alexandra Roth <alroth8@gmail.com>
2017-04-18 09:41:28 -0400

    added category table and column to addon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417190917) do
+ActiveRecord::Schema.define(version: 20170418133834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170417190917) do
     t.integer  "initial_width"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_addons_on_category_id", using: :btree
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "addons", "categories"
 end
