8cea459df758d7347b58c71dbba40a2cb8e637f9
chasms <chasms@chas.ms>
2017-04-18 09:48:57 -0400

    added emojis in seeds file, refactored seeds with loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M1.svg", :initial_height => 300, :initial_width => 300)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M2.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M3.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M4.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M5.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M6.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M7.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M8.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M9.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M10.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/moustaches/M11.svg", :initial_height => 100, :initial_width => 150)
-Addon.create!(:url => "http://chas.ms/olio/Octosteve.svg", :initial_height => 500, :initial_width => 750)
+# moustaches, category='moustaches' category_id=1
+11.times do |i|
+  Addon.create!(url: "http://chas.ms/olio/moustaches/M#{i}.svg", initial_height: 300, initial_width: 300, category_id: 1)
+end
+
+# Octosteve, category='miscellaneous', category_id=3
+Addon.create!(url: "http://chas.ms/olio/Octosteve.svg", :initial_height => 500, :initial_width => 750, category_id: 3)
+
+# emojis, category='emojis', category_id=2
+1837.times do |i|
+  Addon.create!(url: "http://chas.ms/olio/emojis/E#{i}.svg", :initial_height => 100, :initial_width => 100, category_id: 2)
+end
