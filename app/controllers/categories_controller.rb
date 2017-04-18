d7379ffd5ce84031db7d33fa277ac006a8465628
chasms <chasms@chas.ms>
2017-04-18 12:20:20 -0400

    added categories show, renders all addons for that category for use in drawer tabs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,12 +4,23 @@ class CategoriesController < ApplicationController
 		render json: @categories = Category.all
 	end
 
-	def create
-   	 	@category=Category.new(name: params[:name])
-    	if @category.save
-     	 	render json: @category
-    	else
-      		render json: @category.errors.full_messages #status
-    	end
-  	end
+	# def create
+ # 	 	@category = Category.new(name: params[:name])
+  # 	if @category.save
+  #  	 	redirect_to @category
+  # 	else
+  # 		render json: @category.errors.full_messages #status
+  # 	end
+	# end
+
+	def show
+		@category = Category.where(name: params[:name]).first
+		if @category
+			@addons = Addon.where(category_id: @category.id)
+			render json: @addons
+		else
+			render json: @category
+		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	resources :accounts, only: [:index, :create]
 	resources :addons, only: [:index, :show]
-	resources :categories, only: [:index, :create]
+	resources :categories, only: [:index]
+	get '/categories/:name', to: 'categories#show'
 	resources :registrations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
