eae8325f121a991dc553d1c10f33ece224abc306
Alexandra Roth <alroth8@gmail.com>
2017-04-18 10:00:47 -0400

    added categories controller and routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,15 @@
+class CategoriesController < ApplicationController
+
+	def index
+		@categories = Category.all
+	end
+
+	def create
+   	 	@category=Category.new(name: params[:name])
+    	if @category.save
+     	 	render json: @category
+    	else
+      		render json: @category.errors.full_messages #status
+    	end
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-	resources :accounts, only: [:index]
+	resources :accounts, only: [:index, :create]
 	resources :addons, only: [:index, :show]
-
+	resources :categories, only: [:index, :create]
+	resources :registrations, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
